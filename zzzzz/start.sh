2c354e33f546c8da9e159ad03b504bbf83b28908
brunocesarangst <bruno.angst@rede.ulbra.br>
2022-07-15 07:39:47 -0300

    update

--- zzzzz/start.sh
+++ zzzzz/start.sh
@@ -1,7 +1,14 @@
-sleep 5
-
-cd /app
-npx prisma studio
-
-cd /app
-npm start
+#!/bin/sh
+rm -f schema.prisma
+cat <<EOF > schema.prisma
+generator client {
+  provider = "prisma-client-js"
+}
+datasource db {
+  provider = "mysql"
+  url      = env("DATABASE_URL")
+}
+EOF
+node_modules/.bin/prisma db pull
+exec node_modules/.bin/prisma studio
+exec prisma migrate dev --name init
