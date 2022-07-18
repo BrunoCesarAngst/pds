044e60f33037c2541f8ce19fb278cf781072a958
brunocesarangst <bruno.angst@rede.ulbra.br>
2022-07-18 17:21:42 -0300

    up

--- src/sections/Tabs/index.tsx
+++ src/sections/Tabs/index.tsx
@@ -183,7 +183,7 @@ export default function Example() {
                 <div className="pt-4 pb-3 border-t border-gray-700">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <Image className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                      <Image className="h-10 w-10 rounded-full" src="" alt="" />
                     </div>
                     <div className="ml-3">
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
